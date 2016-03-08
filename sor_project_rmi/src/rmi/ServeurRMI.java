ec2f61df22780fad15789f6b7433d7f9ba7a1a12
Hygnir <hygnir@hotmail.fr>
2016-03-08 10:29:42 +0100

    rmi

---
+++ sor_project_rmi/src/rmi/ServeurRMI.java
@@ -0,0 +1,12 @@
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface ServeurRMI  extends Remote {
+
+	public String meth() throws RemoteException ;
+	
+	public String testB() throws RemoteException ;
+}
